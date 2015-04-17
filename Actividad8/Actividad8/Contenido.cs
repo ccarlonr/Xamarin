88f755184523582ab464de2bd5cf358bee9f23c3
ccarlonr <c.carlon@comunidad.unam.mx>
2015-04-16 22:04:03 -0500

    Hice un label con el nombre Contenido Hice un acceso hacia Login

--- Actividad8/Actividad8/Contenido.cs
+++ Actividad8/Actividad8/Contenido.cs
@@ -7,8 +7,34 @@ namespace Actividad8
 	{
 		public Contenido ()
 		{
+			Label texto = new Label {
+				Text = "Contenido",
+				TextColor = Color.Green
+			};
 
+			Button boton = new Button
+			{
+				Text = "Click para navegar al Login"
+			};
+
+			boton.Clicked += (sender, e) => {
+				this.Navigation.PushAsync(new Login());
+			};
+
+			//Stacklayout permite apilar los controles verticalmente
+			StackLayout stackLayout = new StackLayout
+			{
+				Children =
+				{
+					texto,
+					boton
+				}
+				};
+
+
+			//Como esta clase hereda de ContentPage, podemos usar estas propiedades directamente
+			this.Content = stackLayout;
+			this.Padding = new Thickness (5, Device.OnPlatform (20, 5, 5), 5, 5);
 		}
 	}
 }
-
